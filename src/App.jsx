572d171f5d59b446269368d1a58cb2330b28865b
rifkyhilman <rifkyhilman265@gmail.com>
2025-02-04 14:33:25 +0700

    :wrench: Add router
    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 const App = () => {
   return (
-    <>    
-      <Navbar /> 
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>    
+    </Router>
   )
 }
 
