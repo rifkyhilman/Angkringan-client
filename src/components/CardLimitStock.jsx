a60ce749d7b9052be97516e0aeff0ad7660623ae
rifkyhilman <rifkyhilman265@gmail.com>
2025-02-07 21:28:30 +0700

    :lipstick: Update UI scroll cardLimit
    update UI scroll cardLimit

--- src/components/CardLimitStock.jsx
+++ src/components/CardLimitStock.jsx
@@ -8,11 +8,11 @@ import {
 
 const CardLimitStock = () => {
     return(
-        <Card className="flex flex-col my-5">
+        <Card className="flex flex-col my-5 h-[30rem]">
             <CardHeader className="border-b-2 border-b-gray-200">
                 <CardTitle className="text-md font-bold">STOK BARANG TIPIS</CardTitle>
             </CardHeader>
-            <CardContent className="flex-1 pb-0">
+            <CardContent className="flex-1 pb-0 overflow-y-auto">
                 <Card className="mt-5 pr-5 pt-5 h-[8.2rem]">
                 <div className="grid grid-flow-col grid-rows-3 gap-2">
                     <div className="row-span-3 mx-auto">
@@ -32,6 +32,15 @@ const CardLimitStock = () => {
                     </div>
                 </Card>
                 <Card className="mt-5 pr-5 pt-5 h-[8.2rem]">
+                    <div className="grid grid-flow-col grid-rows-3 gap-2">
+                        <div className="row-span-3 mx-auto">
+                            <img src={Kopi} className="w-[100px] h-[100px]" alt="Kopi-aren-top"/>
+                        </div>
+                        <div className="col-span-2 border-b-2 border-b-gray-200 pb-3">Kopi Gula Aren</div>
+                        <div className="col-span-2 text-red-800">Stok: 5 </div>
+                    </div>
+                </Card>
+                <Card className="mt-5 mb-5 pr-5 pt-5 h-[8.2rem]">
                     <div className="grid grid-flow-col grid-rows-3 gap-2">
                         <div className="row-span-3 mx-auto">
                             <img src={Kopi} className="w-[100px] h-[100px]" alt="Kopi-aren-top"/>
--- src/components/CardPie.jsx
+++ src/components/CardPie.jsx
@@ -3,13 +3,11 @@ import { Pie, PieChart } from "recharts"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import {
-//   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
@@ -50,16 +48,13 @@ const CardPie = () => {
           color: "hsl(var(--chart-5))",
         },
       }; 
-      
-    // satisfies ChartConfig
-
+    
     return (
     <Card className="flex flex-col my-5">
-        <CardHeader className="items-center pb-2 border-b-2 border-b-gray-200">
-          <CardTitle>Diagram Produk Terjual</CardTitle>
-          <CardDescription>January - June 2024</CardDescription>
+        <CardHeader className="border-b-2 border-b-gray-200">
+          <CardTitle className="text-md font-bold">PENJUALAN PRODUK SELAMA 1 BULAN</CardTitle>
         </CardHeader>
-        <CardContent className="flex-1 pb-0">
+        <CardContent className="flex-1 pb-0 mt-[4rem] mb-[4rem]">
           <ChartContainer
             config={chartConfig}
             className="mx-auto aspect-square max-h-[250px] pb-0 [&_.recharts-pie-label-text]:fill-foreground"
