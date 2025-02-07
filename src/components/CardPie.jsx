11e2fb3df0b269151c382541ac019e644c5fdf04
rifkyhilman <rifkyhilman265@gmail.com>
2025-02-08 00:56:46 +0700

    :lipstick: Add UI color description chartpie
    add UI color description chartpie

--- src/components/CardPie.jsx
+++ src/components/CardPie.jsx
@@ -1,4 +1,3 @@
-import { TrendingUp } from "lucide-react"
 import { Pie, PieChart } from "recharts"
 import {
   Card,
@@ -16,11 +15,11 @@ import {
 const CardPie = () => {
     
     const chartData = [
-        { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-        { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-        { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-        { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-        { browser: "other", visitors: 90, fill: "var(--color-other)" },
+        { browser: "chrome", visitors: 275, fill: "var(--color-chrome)", color: "hsl(var(--chart-1))"},
+        { browser: "safari", visitors: 200, fill: "var(--color-safari)", color: "hsl(var(--chart-2))"},
+        { browser: "firefox", visitors: 187, fill: "var(--color-firefox)", color: "hsl(var(--chart-3))"},
+        { browser: "edge", visitors: 173, fill: "var(--color-edge)", color: "hsl(var(--chart-4))"},
+        { browser: "other", visitors: 90, fill: "var(--color-other)", color: "hsl(var(--chart-5))"},
       ];
 
     const chartConfig = {
@@ -50,7 +49,7 @@ const CardPie = () => {
       }; 
     
     return (
-    <Card className="flex flex-col my-5">
+    <Card className="flex flex-col my-5 col-span-2">
         <CardHeader className="border-b-2 border-b-gray-200">
           <CardTitle className="text-md font-bold">PENJUALAN PRODUK SELAMA 1 BULAN</CardTitle>
         </CardHeader>
@@ -66,12 +65,19 @@ const CardPie = () => {
           </ChartContainer>
         </CardContent>
         <CardFooter className="flex-col gap-2 text-sm">
-          <div className="flex items-center gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-          </div>
-          <div className="leading-none text-muted-foreground">
-            Showing total visitors for the last 6 months
-          </div>
+            <div className="leading-none text-muted-foreground mb-2 uppercase">
+                januari - februari 2025
+            </div>
+            <div className="flex items-center gap-2 font-medium leading-none">
+                {chartData.map(data => {
+                    return(
+                        <div key={data.browser} className="flex mr-2">
+                            <div className={`bg-[${data.color}] w-3 h-auto mr-1`}></div>
+                            <p>{data.browser}</p>
+                        </div>
+                    )}
+                )}
+            </div>
         </CardFooter>
     </Card>
     )
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -17,7 +17,7 @@ const DashboardPage = () => {
                 <CardSale/>
                 <CardSale/>
             </section>
-            <section className="grid grid-cols-2 gap-6 max-md:grid-cols-1">
+            <section className="grid grid-cols-3 gap-6  max-md:grid-cols-1">
                 <CardPie/>
                 <CardLimitStock/>
             </section>
