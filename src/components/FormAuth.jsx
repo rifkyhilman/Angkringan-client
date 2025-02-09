4758e1536a7642f879474e17704a816b45015ba8
rifkyhilman <rifkyhilman265@gmail.com>
2025-02-10 00:09:35 +0700

    :construction: Auth page ui only
    auth page ui only

--- src/components/FormAuth.jsx
+++ src/components/FormAuth.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button";
@@ -9,38 +10,38 @@ import {
   } from "@/components/ui/card"
 
 const FormAuth = () => {
-    // const handleSubmit = () => {
-    //     console.log("Masuk")
-    // }
-
+    const navigate = useNavigate();
+    const handleSubmit = () => {
+        navigate('/dashboard');
+    }
     return (
-        <div className="container mx-auto flex items-center h-screen">
-            <Card className="shadow w-full">
+        <div className="container mx-auto flex justify-center items-center h-screen">
+            <Card className="shadow w-auto">
                 <CardHeader className="text-center my-7 mx-3">
                     <CardTitle>Masukan Akun</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <form className="space-y-4 md:space-y-6 mt-5" action="#">
+                    <form className="space-y-4 md:space-y-6 my-7" onSubmit={handleSubmit}>
                         <div>
-                            <Label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</Label>
-                            <Input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                            <Label htmlFor="username"className="block mb-2 text-sm font-medium text-gray-900">Nama Pengguna</Label>
+                            <Input type="username" id="username"  placeholder="Masukan Nama" required=""/>
                         </div>
                         <div>
-                            <Label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</Label>
-                            <Input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                            <Label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Kata Sandi</Label>
+                            <Input type="password" id="password" placeholder="••••••••" required=""/>
                         </div>
                         <div className="flex items-center justify-between">
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
-                                    <Input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                                    <Input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required=""/>
                                 </div>
                                 <div className="ml-3 text-sm">
-                                    <Label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</Label>
+                                    <Label for="remember" className="mr-5 text-gray-500 dark:text-gray-300">Ingatkan Saya</Label>
                                 </div>
                             </div>
-                            <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                            <a href="#" className="text-sm ml-5 text-primary-600 hover:underline dark:text-primary-500">Lupa password?</a>
                         </div>
-                        <Button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</Button>
+                        <Button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center">Masuk</Button>
                     </form>
                 </CardContent>
             </Card>
