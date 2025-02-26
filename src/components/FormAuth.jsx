a34a98fc89ba6fc9aa512c9d60b789a0a1bb6ad7
rifkyhilman <rifkyhilman265@gmail.com>
2025-02-26 18:54:47 +0700

    :see_no_evil: Add env variable API
    add env variable API

--- src/components/FormAuth.jsx
+++ src/components/FormAuth.jsx
@@ -41,7 +41,7 @@ const FormAuth = () => {
             setLoading(false);
           };
 
-          const response = await fetch("http://localhost:3000/api/auth/login", {
+          const response = await fetch(`${import.meta.env.VITE_API}/auth/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({email, password}),
