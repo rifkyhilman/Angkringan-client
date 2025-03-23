8ed6aa6c7250bc36256040e6be765e84c4e12134
rifkyhilman <rifkyhilman265@gmail.com>
2025-03-23 23:46:24 +0700

    :construction: Show password
    show password

--- src/components/AuthPage/FormAuth.jsx
+++ src/components/AuthPage/FormAuth.jsx
@@ -15,10 +15,11 @@ import { CircleCheck, CircleX, Loader2 } from "lucide-react";
 
 const FormAuth = () => {
     const navigate = useNavigate();
+    const { toast } = useToast();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const { toast } = useToast();
     const [loading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmit = async(event) =>  {
         event.preventDefault();
@@ -94,6 +95,12 @@ const FormAuth = () => {
         }
       };
 
+      const handleShow = () => {
+        return(
+          setShowPassword(true)
+        )
+      };
+
     return (
         <div className="container mx-auto flex justify-center items-center w-full h-screen max-sm:px-[3rem]">
             <Card className="shadow lg:w-[35%] md:w-[60%] max-sm:w-full">
@@ -105,11 +112,14 @@ const FormAuth = () => {
                         <div className="space-y-4 md:space-y-6 my-7">
                             <div>
                                 <Label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</Label>
-                                <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}  required=""/>
-                            </div>
+                                <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}  required />
+                            </div> 
                             <div>
                                 <Label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Kata Sandi</Label>
-                                <Input type="password" placeholder="••••••••" onChange={(e) => setPassword(e.target.value)} required=""/>
+                                <Input type={showPassword ? "text" : "password"} placeholder="••••••••" onChange={(e) => setPassword(e.target.value)} required />
+                                <button onClick={handleShow}>
+                                  klik ini 
+                                </button>
                             </div>
                             <div>
                                 <a href="#" className="text-sm text-primary-600 hover:underline dark:text-primary-500 max-sm:text-sm">Lupa password?</a>
